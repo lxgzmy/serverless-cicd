d1b8d504c4ed1f3a2eea2b2f0646f96593bbfd14
Jason Liu <lxgzmy@gmail.com>
2018-05-30 14:10:44 +1000

    deploy template

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 cicd autodeployment pipeline for safe deployment
 
 
-aws cloudformation deploy --template-file outputSamTemplate.yaml --stack-name mycicdstack  --capabilities CAPABILITY_IAM
+to deploy run the shell script
+run deploy.sh bucket->serverless-cicd-lxgzmy bucketKey->5faf50da0d822c547b2819efdd670c99 functionName ->mycicdstackTimeFunctionQQ8HEDFP1V63
+
+./deploy.sh serverless-cicd-lxgzmy 5faf50da0d822c547b2819efdd670c99 mycicdstackTimeFunctionQQ8HEDFP1V63
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,20 @@
+
+if [ -z "$1" ]
+  then
+    echo "No artifact supplied"
+    return
+fi
 if [ -z "$2" ]
   then
-    $2= "mycicdstackTimeFunctionQQ8HEDFP1V63"
+    echo "No artifact supplied"
+    return
 fi
-sed 's/{lambda_function_name}/mycicdstackTimeFunctionQQ8HEDFP1V63/g' deployTemplate.yaml > deploy.yaml
-aws cloudformation deploy --template-file deployTemplate.yaml --stack-name mycicdstack  --capabilities CAPABILITY_IAM --parameter-overrides "bucketKey"=$1
+
+if [ -z "$3" ]
+  then
+    $3= "mycicdstackTimeFunctionQQ8HEDFP1V63"
+fi
+sed 's/{lambda_function_name}/'$3'/g' deployTemplate.yaml > deploy.yaml
+aws cloudformation deploy --template-file deploy.yaml --stack-name mycicdstack \
+    --capabilities CAPABILITY_IAM --parameter-overrides \
+    "bucket"=$1 "bucketKey"=$2 
