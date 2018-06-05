c5bcb81acce8d2a60ccbb208e64ef16c81a76dc3
Jason Liu <lxgzmy@gmail.com>
2018-06-05 11:44:04 +1000

    added unit test

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 exports.handler = (event, context, callback) => {
-    console.log("second version");
-    console.log("another change 2");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is 333: version 11' ,
+        body: 'The time in Los Angeles is 333: version 12',
     });
 };
