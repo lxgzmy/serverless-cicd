dc96cd43178b1270ec7edb23998eb8e724b8fa35
Jason Liu <lxgzmy@gmail.com>
2018-06-15 11:48:52 +1000

    version 14

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is 333: version 13',
+        body: 'The time in Los Angeles is 333: version 14',
     });
 };
