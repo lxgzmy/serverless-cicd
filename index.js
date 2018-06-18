a74c1320480d8d3bb06c9df2dd7a8593ffebd3d5
Jason Liu <lxgzmy@gmail.com>
2018-06-18 17:28:06 +1000

    add alert for sam

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is 333: version 14',
+        body: 'The time in Los Angeles is : version 14',
     });
 };
